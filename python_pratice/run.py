8abc9cf012009753349816c5872847877ac43d37
Luciya0604 <39544837+Luciya0606@users.noreply.github.com>
2021-02-02 17:48:03 +0800

    Delete python_pratice directory

--- python_pratice/run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from python_pratice.sendgift import send, show
-
-if __name__ == '__main__':
-    send()
-    show()
--- python_pratice/sendgifts/gift.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# 拥有礼物的标识
-have_gift = False
